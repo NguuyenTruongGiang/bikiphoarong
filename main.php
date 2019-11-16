d746cbba62b72be6c498e136e53de5e15c8f42c4
NguuyenTruongGiang <57828209+NguuyenTruongGiang@users.noreply.github.com>
2019-11-16 15:50:49 +0700

    1

---
+++ main.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+	<title>văn bản pháp luật</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="văn bản pháp luật">
+	<meta name="author" content="Luu Tien Dao">
+	
+	
+</head>
+<body>
+
+
+<?php 
+	
+	
+	echo 'Hello Google App !'; 
+	echo PHP_VERSION; 
+	echo "<br/><br/>"; 
+	
+?>
+
+</body>
+</html>
